5c442db8a6352c21325f372486409d44ad3f5b76
Sam Jacob <samjacob18@gmail.com>
2013-02-24 01:18:51 -0600

    Backup directory is made, and a notification is sent and logged if the directory doesn't exist

--- ServerBackup.py
+++ ServerBackup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python2
-import LogUncaught, ConfigParser, logging, os
+import LogUncaught, ConfigParser, logging, PushBullet, os
+from time import localtime, strftime
 
 sbConfig = ConfigParser.RawConfigParser()
 sbConfig.read('scripts.cfg')
@@ -9,7 +10,7 @@ sbLFH = logging.FileHandler(sbConfig.get('ServerBackup', 'log_location'))
 sbLFH.setLevel(logging.DEBUG)
 
 # Logger Formatter
-sbLFORMAT = logging.Formatter('[%(asctime)s | %(levelname)s]  - %(message)s')
+sbLFORMAT = logging.Formatter('[%(asctime)s | %(levelname)s] - %(message)s')
 sbLFH.setFormatter(sbLFORMAT)
 
 # Logger
@@ -22,5 +23,13 @@ sbLogger.info("Script has begun")
 sbLocation = sbConfig.get('ServerBackup', 'backup_location')
 databasePass = sbConfig.get('Database', 'password')
 lbLocation = sbConfig.get('LogBackup', 'backup_location')
+date = strftime("%m_%d_%Y_%H_%M_%S", localtime())
+sbFolder = sbLocation + "backup_" + date + "/"
 
+os.makedirs(sbFolder)
 
+if not os.path.exists(sbFolder):
+   message = "Folder, \"%s\", couldn't be made" % sbFolder
+   sbLogger.critical(message)
+   PushBullet.sendPushNote({'id':PushBullet.getPushDevicesIds(), 'title':"Server Backup Error", 'message':message})
+   exit(message)
