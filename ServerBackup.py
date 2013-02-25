7ed83fb6fab5a2488653d1084a7cf0669be2a018
Sam Jacob <samjacob18@gmail.com>
2013-02-25 12:35:35 -0600

    MySQL dump was added

--- ServerBackup.py
+++ ServerBackup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2
-import LogUncaught, ConfigParser, logging, PushBullet, os
+import LogUncaught, ConfigParser, logging, PushBullet, os, time, traceback, MySQLdb as mdb
 from time import localtime, strftime
+from hurry.filesize import size
 
 sbConfig = ConfigParser.RawConfigParser()
 sbConfig.read('scripts.cfg')
@@ -18,6 +19,15 @@ sbLogger = logging.getLogger("serverbackup_logger")
 sbLogger.setLevel(logging.DEBUG)
 sbLogger.addHandler(sbLFH)
 
+def sendPush(title, message):
+	PushBullet.sendPushNote({'id':PushBullet.getPushDevicesIds(), 'title':title, 'message':message})
+
+def logCritical(message, exception):
+   sbLogger.critical(message)
+   sbLogger.critical(exception)
+   sendPush("Server Backup Error", message)
+   exit(message)
+
 sbLogger.info("Script has begun")
 
 sbLocation = sbConfig.get('ServerBackup', 'backup_location')
@@ -25,11 +35,33 @@ databasePass = sbConfig.get('Database', 'password')
 lbLocation = sbConfig.get('LogBackup', 'backup_location')
 date = strftime("%m_%d_%Y_%H_%M_%S", localtime())
 sbFolder = sbLocation + "backup_" + date + "/"
+sqlDumpName = sbFolder + "backup_sql_" + date + ".sql"
 
 os.makedirs(sbFolder)
 
 if not os.path.exists(sbFolder):
-   message = "Folder, \"%s\", couldn't be made" % sbFolder
-   sbLogger.critical(message)
-   PushBullet.sendPushNote({'id':PushBullet.getPushDevicesIds(), 'title':"Server Backup Error", 'message':message})
-   exit(message)
+   logCritical("Folder, \"%s\", couldn't be made" % sbFolder)
+   
+try:
+	sbLogger.info("Starting MySQL dump")
+	startTime = time.time()
+	dbConn = mdb.connect('localhost', 'root', databasePass)
+	sbLogger.info("Connected to MySQL Server")
+	cur = dbConn.cursor()
+	sbLogger.info("Putting read lock on tables")
+	cur.execute("FLUSH TABLES WITH READ LOCK;")
+	sbLogger.info("Taking MySQL dump")
+	os.system("mysqldump -uroot -p" + databasePass + " --single-transaction --routines --triggers --all-databases > " + sqlDumpName)
+	sbLogger.info("Finished taking MySQL dump")
+	cur.execute("UNLOCK TABLES")
+	sbLogger.info("Unlocked tables")
+	dbConn.close()
+	sbLogger.info("Disconnected from MySQL Server")
+	sbLogger.info("Finished with MySQL dump")
+	endTime = str(time.time() - startTime)
+	dumpSize = size(os.path.getsize(sqlDumpName))
+	sbLogger.info("Took %s seconds to execute MySQL dump" % endTime)
+	sbLogger.info("MySQL dump was %s" % dumpSize)
+	sendPush("Server Backup [MySQL Dump]", "MySQL dump was successful, took %s seconds with a dump filesize of %s" % (endTime, dumpSize))
+except Exception, e:
+	logCritical("An error occured during MySQL dump", traceback.format_exc())
